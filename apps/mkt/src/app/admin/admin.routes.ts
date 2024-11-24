5087a7bec37fa8a4d53c7ffe5d0439fbd4e9d9c5
Daniel Weiß <daniel.weiss@grollmus.de>
2024-11-24 11:13:11 +0100

    feat(): add tournament page

--- apps/mkt/src/app/admin/admin.routes.ts
+++ apps/mkt/src/app/admin/admin.routes.ts
@@ -1,11 +1,15 @@
 import { Route } from '@angular/router';
 import { AdminStartpagePage } from './pages/startpage/admint-startpage.page';
 import { AdminShell } from './layout/admin.shell';
+import { TournamentPage } from './pages/tournament/tournament.page';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: AdminShell,
-    children: [{ path: '', component: AdminStartpagePage }],
+    children: [
+      { path: '', component: AdminStartpagePage },
+      { path: 'tournament', component: TournamentPage },
+    ],
   },
 ];
---
+++ apps/mkt/src/app/admin/pages/tournament/tournament.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tournament',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tournament.page.html',
+})
+export class TournamentPage {}
