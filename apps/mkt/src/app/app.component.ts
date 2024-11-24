bee4325725aec87f0a2f72c51987cb7d2aec81bf
Daniel Weiß <daniel.weiss@grollmus.de>
2024-11-24 09:34:51 +0100

    add settings.json

--- apps/mkt/src/app/app.component.ts
+++ apps/mkt/src/app/app.component.ts
@@ -8,4 +8,6 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  foo = 'bar';
+}
