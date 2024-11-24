416f855b0273ababd806ad8e1c91d94cccb1df97
Daniel Weiß <daniel.weiss@grollmus.de>
2024-11-24 09:32:04 +0100

    remove dummy nx component, add tailwindcss

--- apps/mkt/src/app/app.component.spec.ts
+++ apps/mkt/src/app/app.component.spec.ts
@@ -1,25 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome mkt');
-  });
-
-  it(`should have as title 'mkt'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mkt');
   });
 });
